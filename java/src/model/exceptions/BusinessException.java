a2f47a87ad61cf143f292bac13f7b45a99686a05
ellisroberta <ellisroberta@hotmail.com>
2024-11-01 16:42:31 -0300

    organizacao das pastas, edicao do .gitignore e realizacao do exercicio

---
+++ java/src/model/exceptions/BusinessException.java
@@ -0,0 +1,12 @@
+package model.exceptions;
+
+import java.io.Serial;
+
+public class BusinessException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public BusinessException(String msg) {
+        super(msg);
+    }
+}
